f9409f0bb5c28d1d9603c91b0977b756fe3c6919
Dias Zholgali <zolgalidias@gmail.com>
2026-05-18 17:17:37 +0500

    Stage 2: students list + detail navigation

--- SchoolApp/MainPage.xaml.cs
+++ SchoolApp/MainPage.xaml.cs
@@ -14,4 +14,8 @@ public partial class MainPage : ContentPage
         _count++;
         StatusLabel.Text = $"Button clicked: {_count} times";
     }
+    private async void OnOpenStudentsClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(StudentsPage));
+    }
 }
